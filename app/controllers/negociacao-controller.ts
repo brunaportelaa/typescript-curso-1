87552d121656c1ba36a0db00a8524a7282563d42
brunaportelaa <brunaporteladj@gmail.com>
2024-08-13 23:53:49 -0300

    Updated negociacao-controller.ts

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,5 @@
+import { Negociacao } from "../models/negociacao.js";
+
 export class NegociacaoController {
     private inputData;
     private inputQuantidade;
@@ -7,5 +9,15 @@ export class NegociacaoController {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
+    }
+
+    adiciona() {
+        const negociacao = new Negociacao(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        )
+        
+        console.log(negociacao)
     }
 }
